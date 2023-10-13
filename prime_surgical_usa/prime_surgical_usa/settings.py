53ef5f458b2eaa9ba3e53a3166f99c0c04411235
Alex Paino <alphaprime@Alexs-MBP.attlocal.net>
2023-10-13 12:07:01 -0400

    login ts

--- prime_surgical_usa/prime_surgical_usa/settings.py
+++ prime_surgical_usa/prime_surgical_usa/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
 import django_heroku
 
 
@@ -80,13 +82,13 @@ WSGI_APPLICATION = "prime_surgical_usa.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST') ,
-        'PORT': '5432'
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
