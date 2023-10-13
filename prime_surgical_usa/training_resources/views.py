3f79418b37bbb7f7bb9b49a2d5710b9eadf5bdfe
Alex Paino <alphaprime@Alexs-MBP.attlocal.net>
2023-10-13 12:11:01 -0400

    login ts

--- prime_surgical_usa/training_resources/views.py
+++ prime_surgical_usa/training_resources/views.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
+#@login_required
 def index(request):
     return render(request, 'training_resources/new_teammate_training.html')
 
-@login_required
+#@login_required
 def anatomy_fundamentals(request):
     return render(request, 'training_resources/anatomy_fundamentals.html')
 
-@login_required
+#@login_required
 def cardiac_surgical_intervention(request):
     return render(request, 'training_resources/cardiac_surgical_intervention.html')
